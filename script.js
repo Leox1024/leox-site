ef61f3dab1ef8c346d2c1eb79960a1baa3b0d646
L3ox64 <93765605+l3ox64@users.noreply.github.com>
2024-02-11 22:22:20 +0100

    new-html

---
+++ script.js
@@ -0,0 +1,30 @@
+const buttons = document.querySelectorAll(".card-buttons button");
+const sections = document.querySelectorAll(".card-section");
+const card = document.querySelector(".card");
+
+const handleButtonClick = e => {
+  const targetSection = e.target.getAttribute("data-section");
+  const section = document.querySelector(targetSection);
+  targetSection !== "#about" ?
+  card.classList.add("is-active") :
+  card.classList.remove("is-active");
+  card.setAttribute("data-state", targetSection);
+  sections.forEach(s => s.classList.remove("is-active"));
+  buttons.forEach(b => b.classList.remove("is-active"));
+  e.target.classList.add("is-active");
+  section.classList.add("is-active");
+};
+
+buttons.forEach(btn => {
+  btn.addEventListener("click", handleButtonClick);
+});
+
+
+
+function btc() {
+    var key = "bc1qrr334q6jnux5zt9al9tnwsp3pdpl50zls89q7q";
+
+    navigator.clipboard.writeText(key);
+    alert("Address copied to clipboard!");
+
+}
